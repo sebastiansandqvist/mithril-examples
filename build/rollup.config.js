a8256b3530b7d5cbbae7e8c6bd0a9ec78137644c
Sebastian Sandqvist <seb@sparque.me>
2017-02-11 23:05:03 -0800

    version

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -5,7 +5,7 @@ import uglify from 'rollup-plugin-uglify';
 
 module.exports = {
 	entry: 'src/index.js',
-	dest: 'public/bundle.06.js',
+	dest: 'public/bundle.07.js',
 	format: 'iife',
 	plugins: [
 		nodeResolve({ browser: true }),
