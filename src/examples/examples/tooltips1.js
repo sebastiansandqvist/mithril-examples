9432ef1432abaa1d2b71597e00ee97f7a98bf8ae
Sebastian Sandqvist <seb@sparque.me>
2017-04-27 22:36:25 -0700

    update tooltips example

--- src/examples/examples/tooltips1.js
+++ src/examples/examples/tooltips1.js
@@ -119,8 +119,8 @@ const css = codeString.css(
 }`);
 
 export const code = [
-  { id: 'es5', code: es5 },
   { id: 'es6', code: es6 },
+  { id: 'es5', code: es5 },
   // { id: 'jsx', code: jsx },
   { id: 'css', code: css }
 ];
