f864a015ffc66ca86590196f250cc434bf343f79
Sebastian Sandqvist <seb@sparque.me>
2017-04-29 11:32:26 -0700

    use correct indentation

--- src/examples/demos/stopwatch/demo.js
+++ src/examples/demos/stopwatch/demo.js
@@ -1,34 +1,34 @@
 import m from 'mithril';
 
 function stopwatchModel() {
-  return {
-    interval: null,
-    seconds: 0
-  };
+	return {
+		interval: null,
+		seconds: 0
+	};
 }
 
 const actions = {
-  increment: function(model) {
-    model.seconds++;
-    m.redraw();
-  },
-  start: function(model) {
-    model.interval = setInterval(actions.increment, 1000, model);
-  },
-  stop: function(model) {
-    model.interval = clearInterval(model.interval);
-  }
+	increment: function(model) {
+		model.seconds++;
+		m.redraw();
+	},
+	start: function(model) {
+		model.interval = setInterval(actions.increment, 1000, model);
+	},
+	stop: function(model) {
+		model.interval = clearInterval(model.interval);
+	}
 };
 
 export default function Stopwatch() {
-  const model = stopwatchModel();
-  actions.start(model);
-  return {
-    view: function() {
-      return m('span', 'Timer: ' + model.seconds);
-    },
-    onremove: function() {
-      actions.stop(model);
-    }
-  };
+	const model = stopwatchModel();
+	actions.start(model);
+	return {
+		view: function() {
+			return m('span', 'Timer: ' + model.seconds);
+		},
+		onremove: function() {
+			actions.stop(model);
+		}
+	};
 }
--- src/examples/demos/stopwatch3/demo.js
+++ src/examples/demos/stopwatch3/demo.js
@@ -2,17 +2,17 @@ import m from 'mithril';
 import Stopwatch from '../stopwatch2/demo.js';
 
 function App() {
-  return [
-    m('strong', 'Stopwatch 1: '),
-    m(Stopwatch),
-    m('hr'),
-    m('strong', 'Stopwatch 2: '),
-    m(Stopwatch)
-  ];
+	return [
+		m('strong', 'Stopwatch 1: '),
+		m(Stopwatch),
+		m('hr'),
+		m('strong', 'Stopwatch 2: '),
+		m(Stopwatch)
+	];
 }
 
 export default {
-  view() {
-    return App();
-  }
+	view() {
+		return App();
+	}
 };
